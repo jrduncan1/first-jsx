9f0927a30d94cf2dbab6d284f45f6252cec937d4
jrduncan1 <dunc5201@gmail.com>
2022-01-12 12:11:54 -0700

    first-jsx assignment

---
+++ src/components/MainClass.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class MainClass extends Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <h1>Hello Dojo!</h1>
+                </div>
+                <div>
+                    <h4>Things I need to do:</h4>
+                </div>
+                <div>
+                    <li>Learn React</li>
+                    <li>Climb Mt. Everest</li>
+                    <li>Run a marathon</li>
+                    <li>Feed the dogs</li>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default MainClass;
